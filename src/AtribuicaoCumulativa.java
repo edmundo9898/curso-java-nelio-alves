26c8b5c9486dbaaf54fb13494b76a993f62aab32
EdmundoPC <edmundo.roberto@hotmail.com>
2026-03-18 16:04:30 -0300

    Aulas Atribuição Cumulativa  e SwitchCase

---
+++ src/AtribuicaoCumulativa.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AtribuicaoCumulativa {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int minutos = sc.nextInt();
+        double conta = 50.0;
+
+        if( minutos > 100){
+            // mesma coisa que conta = conta + ......
+            conta += (minutos - 100) * 2.0;
+        }
+
+        System.out.printf("Valor da conta = R$ %.2f%n", conta);
+
+
+        sc.close();
+    }
+}
